a0d9702fffed146262f95f05dc2d3ea0eb901d0e
RoySamson-stack <rsam9009@gmail.com>
2021-09-17 22:05:11 +0300

    adding the business logic for reading the option choosen by the user

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+
 $(document).ready(function(){
   $("#order_btn").click(function(){
     $("#order_form").toggle();
   })
-})
+});
+
+const size = document.querySelector(`[id="size"]`);
+const crust = document.querySelector(`[id="crust"]`);
+const toppings = document.querySelector(`[id="toppings"]`);
+var amount = document.getElementById('pizza_amnt').value;
+
+
+size.addEventListener(`change`, (e) => {
+  // log(`e.target`, e.target);
+  const select = e.target;
+  const value = select.value;
+  const desc = select.selectedOptions[0].text;
+  console.log( desc);
+});
+crust.addEventListener(`change`, (e) => {
+  // log(`e.target`, e.target);
+  const select = e.target;
+  const value = select.value;
+  const desc = select.selectedOptions[0].text;
+  console.log( desc);
+});
+toppings.addEventListener(`change`, (e) => {
+  // log(`e.target`, e.target);
+  const select = e.target;
+  const value = select.value;
+  const desc = select.selectedOptions[0].text;
+  console.log( desc);
+});
