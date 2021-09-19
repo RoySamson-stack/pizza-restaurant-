e00afa32cd9cbe6bae4e401c521712337e81fc0e
RoySamson-stack <rsam9009@gmail.com>
2021-09-19 15:47:55 +0300

    making the appending for the form for another order

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-function Pizza(sizes, top, crust) {
-  this.sizes= sizes;
+function Order(sizes, top, crust) {
+  this.sizes = sizes;
   this.top = top;
   this.crust = crust;
 }
@@ -12,55 +11,71 @@ function Pizza(sizes, top, crust) {
 // this.toppings = toppings;
 // this.price = price
 // }
-Pizza.prototype.fullPizza = function(){
+Order.prototype.fullPizza = function () {
   return this.sizes + " " + this.top + " " + this.crust;
 }
-function Location(street, house, number){
-this.street = street;
-this.house = house;
-this.number = number;
+
+function Location(street, house, number) {
+  this.street = street;
+  this.house = house;
+  this.number = number;
 }
 
-Location.prototype.fullAddress = function(){
-return this.street + " " + this.house + " " + this.number + " ";
+Location.prototype.fullAddress = function () {
+  return this.street + " " + this.house + " " + this.number + " ";
 }
-const size = document.querySelector(`[id="size"]`);
-const crust = document.querySelector(`[id="crust"]`);
-const toppings = document.querySelector(`[id="toppings"]`);
-var amount = document.getElementById('pizza_amnt').value;
 
-size.addEventListener(`change`, (e) => {
-  const select = e.target;
-  const value = select.value;
-  const desc = select.selectedOptions[0].text;
-  console.log(desc);
-});
-crust.addEventListener(`change`, (e) => {
-  const select = e.target;
-  const value = select.value;
-  const desc = select.selectedOptions[0].text;
-  console.log(desc);
-});
-toppings.addEventListener(`change`, (e) => {
-  const select = e.target;
-  const value = select.value;
-  const desc = select.selectedOptions[0].text;
-  console.log(desc);
-});
 $(document).ready(function () {
   $("#order_btn").click(function () {
     $("#order_form").show();
   });
-  // $("#place_order").click(function () {
-  //   $("#delivery_form").toggle();
+  //  $("#place_order").click(function () {
+  //    $("#delivery_form").toggle();
   // });
   // $("#cancel_btn").click(function () {
   //   $("#delivery_form").hide();
   // });
-  $('').click(function() {
-    $('#').append()
-  })
-})
-function reset(){
+  $("#add_order").click(function () {
+    $("#order_form").append(
+      '<form class="" id="order_form">' +
+      ' <div class="">' +
+      " <h3>Pizza size</h3>" +
+      '<select class="custom-select" id="size">' +
+      '<option value="">small -- 250</option>' +
+      '<option value="">medium -- 500</option>' +
+      '<option value="">large -- 1000</option>' +
+      '<option value="">extra large --1250</option>' +
+      "</select>" +
+      " </div>" +
+      " <div class=''>" +
+      "<h3>Pizza crust</h3>" +
+      '<select class="custom-select" id="crust">' +
+      " <option value=''>crispy</option>" +
+      " <option value=''>stuffed </option>" +
+      "<option value=''>gluten free </option>" +
+      " <option value=''>chicago-style deep dish </option>" +
+      "  </select>" +
+      " </div>" +
+      "  <div class=''>" +
+      "<h3>pizza toppings</h3>" +
+      '<select class="custom-select" id="toppings">' +
+      "<option value=''>vegan -- 220</option>" +
+      " <option value=''>pepperoni -- 150</option>" +
+      " <option value=''>mushroom -- 120</option>" +
+      "  <option value=''>pineapple -- 80</option>" +
+      '</select' +
+      "  </div>" +
+      "  <div class=''>" +
+      "<label>Amount</label>" +
+      '<input type="text" id="pizza_amnt" placeholder="">' +
+      " </div>" +
+      '   <input type="submit" value="Place order" id="place_order" class="btn btn-primary">' +
+      "</form>"
+    )
+  });
   
+})
+
+function reset() {
+
 }
