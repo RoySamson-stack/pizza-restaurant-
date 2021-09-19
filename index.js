0b63416b6ec68da5dfc396dd90e6221c66c9e0fb
RoySamson-stack <rsam9009@gmail.com>
2021-09-19 14:00:49 +0300

    making the prototype for the order and for the location of delivery

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-$(document).ready(function () {
-  $("#order_btn").click(function () {
-    $("#order_form").show();
-  });
-  // $("#place_order").click(function () {
-  //   $("#delivery_form").toggle();
-  // });
-  // $("#cancel_btn").click(function () {
-  //   $("#delivery_form").hide();
-  // })
-});
 
-function pizza(sizes, top, crust) {
-  this.sizes= [];
-  this.top = [];
+function Pizza(sizes, top, crust) {
+  this.sizes= sizes;
+  this.top = top;
   this.crust = crust;
 }
-function sizes(size, price){
-  this.size = size;
-  this.price = price;
-}
+// function Sizes(size, price){
+//   this.size = size;
+//   this.price = price;
+// }
 // function top(toppings, price){
 // this.toppings = toppings;
 // this.price = price
 // }
+Pizza.prototype.fullPizza = function(){
+  return this.sizes + " " + this.top + " " + this.crust;
+}
+function Location(street, house, number){
+this.street = street;
+this.house = house;
+this.number = number;
+}
 
+Location.prototype.fullAddress = function(){
+return this.street + " " + this.house + " " + this.number + " ";
+}
 const size = document.querySelector(`[id="size"]`);
 const crust = document.querySelector(`[id="crust"]`);
 const toppings = document.querySelector(`[id="toppings"]`);
 var amount = document.getElementById('pizza_amnt').value;
-
 
 size.addEventListener(`change`, (e) => {
   const select = e.target;
@@ -48,6 +47,20 @@ toppings.addEventListener(`change`, (e) => {
   const desc = select.selectedOptions[0].text;
   console.log(desc);
 });
-function calculation(){
+$(document).ready(function () {
+  $("#order_btn").click(function () {
+    $("#order_form").show();
+  });
+  // $("#place_order").click(function () {
+  //   $("#delivery_form").toggle();
+  // });
+  // $("#cancel_btn").click(function () {
+  //   $("#delivery_form").hide();
+  // });
+  $('').click(function() {
+    $('#').append()
+  })
+})
+function reset(){
   
 }
