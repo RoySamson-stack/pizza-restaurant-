444ade3846c21df5ad93ebfd8161272d672ca49c
RoySamson-stack <rsam9009@gmail.com>
2021-09-21 18:06:52 +0300

    addin the fucntionality for the delivery

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ $(document).ready(function () {
   $("#nt_deliver").click(function () {
     $(".locate_del").hide()
   });
-  $("#place_order").click(function(){
+  $("#place_order").click(function () {
     $("#show_order").show();
   });
   $("#cancel_order").click(function () {
     $("#show_order").hide();
     $("#order_form").hide();
   });
-  
+
 })
 const showOrder = $('#show_order');
+const showLocation = $("#delivery");
 const price = {
   small: 500,
   meduim: 700,
@@ -32,12 +33,21 @@ $("form#order_form").submit(function (event) {
     top: ord.get("toppings"),
     amount: ord.get("amount"),
     cost: parseInt(price[ord.get("size")]) * (price[ord.get("amount")]),
-    
+
   }
   const location = {
     area: ord.get("area"),
-street: ord.get("street"),
-house: ord.get("house_no")
+    street: ord.get("street"),
+    house: ord.get("house_no")
+  };
+  if ($("#deliver") == this.checked) {
+      showLocation.append(`
+      <ul id="delivery">
+      <li class="size"> to be delivered  to ${location.area}</li>
+      <li class="toppings">${location.street}</li>
+      <li class="crust">${location.house} pizza.</li>
+    </ul>
+      `)
   }
   showOrder.append(`
     <ul id="full_order">
